e631363467c40e97908af033ce53a06fb18162f4
Developersokkrey <72006616+Developersokkrey@users.noreply.github.com>
2023-12-19 11:51:13 +0700

    CROSS JOIN
     CROSS JOIN

--- T-SQL Query.sql
+++ T-SQL Query.sql
@@ -8,3 +8,11 @@ INNER JOIN(
          ) AS RD 
          ON R.ReceiptID = RD.ReceiptID
 ) TR ON TR.ReceiptID = R.ReceiptID
+
+
+--------------------------------  CROSS JOIN ------------------------------------
+SELECT C.*,U.[Name] AS UserName FROM CUSMER C
+         CROSS JOIN UERACC U
+
+SELECT C.*,U.[Name] AS UserName FROM CUSMER C
+         CROSS JOIN BRANCH U
