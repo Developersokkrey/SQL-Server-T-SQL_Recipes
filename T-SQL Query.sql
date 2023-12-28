e6cfc4bd5ce1de598612b389414db2f39820d964
Developersokkrey <72006616+Developersokkrey@users.noreply.github.com>
2023-12-28 12:04:23 +0700

    Update T-SQL Query.sql

--- T-SQL Query.sql
+++ T-SQL Query.sql
@@ -16,3 +16,31 @@ SELECT C.*,U.[Name] AS UserName FROM CUSMER C
 
 SELECT C.*,U.[Name] AS UserName FROM CUSMER C
          CROSS JOIN BRANCH U
+
+
+-------------------------------- UPDATE JOIN SELECT LAST ------------------------------------
+    UPDATE WS SET WS.InStock = IA.CumulativeQty, WS.CumulativeValue = IA.CumulativeValue
+    FROM tbWarehouseSummary WS
+    INNER JOIN tbInventoryAudit IA ON WS.WarehouseID = IA.WarehouseID AND WS.ItemID = IA.ItemID AND IA.ID IN(
+    SELECT _IA.ID FROM
+    ( 
+    	SELECT MAX(IA.ID) AS ID, IA.ItemID AS ItemID FROM tbInventoryAudit IA
+    	 JOIN tbWarehouseSummary WS ON IA.WarehouseID = WS.WarehouseID AND IA.ItemID = WS.ItemID
+    	--WHERE IA.ItemID = 147 AND IA.WarehouseID = 1
+    	GROUP BY IA.ItemID
+    ) AS _IA)
+    
+-------------------------------- SELECT NESTED JSON ------------------------------------
+SELECT
+    ent.Id AS 'Id',
+    ent.Name AS 'Name',
+    ent.Age AS 'Age',
+    EMails = (
+        SELECT
+            Emails.Id AS 'Id',
+            Emails.Email AS 'Email'
+        FROM EntitiesEmails Emails WHERE Emails.EntityId = ent.Id
+        FOR JSON PATH
+    )
+FROM Entities ent
+FOR JSON PATH
